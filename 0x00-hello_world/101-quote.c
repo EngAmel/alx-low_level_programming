53ccf2a3ada3eed0db6b99972b9afe49de649c82
EngAmel <amel.abdululazim.se@gmail.com>
2023-09-07 19:06:04 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <unistdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:Printting program
+ *
+ * Return:Always 0 (Success)
+ *
+*/
+
+int main(void)
+{
+	char qts[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1.qts.59);
+	return (1);
+}
