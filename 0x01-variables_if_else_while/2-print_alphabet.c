ee9a82b6ea893c77436ab4e736eeb033a3a6f2b0
EngAmel <amel.abdululazim.se@gmail.com>
2023-09-08 14:51:31 -0700

    char

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:C programm use printf function
+ *
+ * Return:Always 0 (Success)
+ *
+*/
+
+int main(void)
+{
+	for (char ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar (ch);
+	}
+	putchar (\n);
+	return (0);
+
+}
