bf9f06e7667d5a651180585b10788735124cfbc0
EngAmel <amel.abdululazim.se@gmail.com>
2023-09-08 16:08:31 -0700

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:C programm use printf function
+ *
+ * Return:Always 0 (Success)
+ *
+*/
+
+int main(void)
+{
+	for (int n = 0; n <= 9; n++)
+	{
+		printf("%i", n);
+	}
+	printf('\n');
+	return (0);
+
+}
