b470415f196b38a76a2c4f4125b54aa9ab060002
EngAmel <amel.abdululazim.se@gmail.com>
2023-09-08 19:16:04 -0700

    commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:C programm use printf function
+ *
+ * Return:Always 0 (Success)
+ *
+*/
+
+int main(void)
+{
+	int n1, n;
+
+	for (n1 = 0; n1 <= 99; n1++)
+	{
+		n = n1;
+		for (n = 0; n <= 9; n++)
+		{
+			if (n != n1)
+			{
+				putchar((n1 / 10 + 48));
+				putchar((n1 % 10 + 48));
+				putchar(' ');
+				putchar(n / 10 + 48);
+				putchar(n % 10 + 48);
+				if  (n1 != 98 || n != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
