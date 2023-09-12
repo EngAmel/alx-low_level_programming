f8c2dadc7ee98a3f0e4c0984656a6b8553052069
EngAmel <amel.abdululazim.se@gmail.com>
2023-09-12 15:48:06 -0700

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:C programm use printf function
+ *
+ * Return:Always 0 (Success)
+ *
+*/
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
