c65ebe4623e2273e1d6c15357abe2e0a4d08e0a2
EngAmel <amel.abdululazim.se@gmail.com>
2023-09-12 20:06:08 -0700

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,11 +8,10 @@
 void times_table(void)
 {
 	int num, mult, prod;
-	
-	for(num = 0; num <= 9; num++)
-	{
-		_putchar(48);
-		for(mult = 1; mult <= 9; mult++)
+
+	for (num = 0; num <= 9; num++)
+	{	_putchar(48);
+		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
@@ -23,7 +22,6 @@ void times_table(void)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + 48);
-			
 			_putchar((prod % 10) + 48);
 		}
 		_putchar('\n');
