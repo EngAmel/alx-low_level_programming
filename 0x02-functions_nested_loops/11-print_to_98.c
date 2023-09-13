1e10f8f6bec202907336d60a06b01f178a4717eb
EngAmel <amel.abdululazim.se@gmail.com>
2023-09-13 07:57:52 -0700

    commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include"main.h"
+
+/**
+ * print_to_98 - print to 98
+ *
+ * @n:number will be start with
+ * Return:Always 0 (Success)
+*/
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+		for (i = n; i <= 98; i++)
+		{
+			printf("%i", i);
+			printf(",");
+			printf(" ");
+		}
+	else
+		for (i = n; i >= 98; i--)
+		{
+			printf("%i", i);
+			printf(",");
+			printf(" ");
+		}
+	putchar('\n');
+	return (0);
+}
