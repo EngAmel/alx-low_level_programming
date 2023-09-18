e75c57ca2e59cbd7e3975bba81e91fd5aef97bba
EngAmel <amel.abdululazim.se@gmail.com>
2023-09-18 13:00:35 -0700

    task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,13 +2,11 @@
 
 /**
  * reset_to_98- reset 98
- *
+ *@n: pointer
  * Return: nothing.
  */
 
 void reset_to_98(int *n)
 {
-	printf("Value of 'n': %p\n", n);
-	printf("Address of 'n': %p\n", &n);
 	*n = 98;
 }
