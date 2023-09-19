a972f71840a33b0c807b908ae2453d978b10ae6d
EngAmel <amel.abdululazim.se@gmail.com>
2023-09-18 17:05:46 -0700

    task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_print_rev - print rev
+ * @s: pointer
+ */
+
+void print_rev(char *s)
+{
+	int i, j, x = 0, y;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		x = x + 1;
+	}
+	for (j = x; j >= 0; j--)
+	{
+		_putchar(str[j]);
+	}
+}
