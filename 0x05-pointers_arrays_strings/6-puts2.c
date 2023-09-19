ada7085352f14207d609cc5ed59fa3efd504f119
EngAmel <amel.abdululazim.se@gmail.com>
2023-09-19 07:44:37 -0700

    task

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,9 +10,9 @@ void puts2(char *str)
 	int i;
 
 	_putchar(str[0]);
-	for (i = 1; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i += 2)
 	{
-		_putchar(str[i + 1]);
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
