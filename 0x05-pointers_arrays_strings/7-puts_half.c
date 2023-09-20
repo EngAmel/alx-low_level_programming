11b7b029b9a44cebfaf766cb37e567a0515f7eac
EngAmel <amel.abdululazim.se@gmail.com>
2023-09-19 17:28:30 -0700

    task

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,12 +12,20 @@ void puts_half(char *str)
 	for (length = 0; str[length] != '\0'; length++)
 		length = length + 1;
 	if (length % 2 != 0)
-		start = (length - 1) / 2;
-	else
-		start = length / 2;
-	for (i = start; i < length; i++)
 	{
-		_putchar(str[i]);
+		start = (length - 1) / 2;
+		for (i = start; i < length - 1; i++)
+        {
+                _putchar(str[i + 1]);
+        }
+	}
+	else if (length % 2 == 0)
+	{
+			start = length / 2;
+			for (i = start; i < length; i++)
+			{
+				_putchar(str[i]);
+			}
 	}
 	_putchar('\n');
 }
