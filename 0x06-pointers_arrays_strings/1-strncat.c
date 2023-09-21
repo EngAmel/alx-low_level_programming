72b2679675ae7a5856dfeb3d690b6dd96d2d4385
EngAmel <amel.abdululazim.se@gmail.com>
2023-09-21 05:09:37 -0700

    task

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * _strcat- connect
+ * _strncat- connect
  * @dest:first string
  * @src:second string
  *
  * Return: strng.
  */
 
-char *_strcat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	int count1, count2;
 
 	for (count1 = 0; dest[count1] != '\0'; count1++)
 		;
-	for (count2 = 0; src[count2] != '\0' ; count2++)
+	for (count2 = 0; src[count2] != '\0' && count2 < n ; count2++)
 	{
 		dest[count1] = src[count2];
 		count1++;
