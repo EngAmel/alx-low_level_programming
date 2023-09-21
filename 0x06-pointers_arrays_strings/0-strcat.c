f3f690654025c6bd4ac36af65469980a537c59d4
EngAmel <amel.abdululazim.se@gmail.com>
2023-09-21 04:39:14 -0700

    task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,8 +12,13 @@ char *_strcat(char *dest, char *src)
 {
 	int count1, count2;
 
-	for (count1 = 0; dest[count1]; count1++)
-	for (count2 = 0; src[count2]; count++)
-		dest[count1++] = src[count2];
+	for (count1 = 0; dest[count1] != '\0'; count1++)
+		;
+	for (count2 = 0; src[count2] != '\0'; count2++)
+	{
+		dest[count1] = src[count2];
+		count1++;
+	}
+	dest[count1] = '\0';
 	return (dest);
 }
