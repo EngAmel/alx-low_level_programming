dcff70a930e97ff0b98275f4deeaed197a1164d8
EngAmel <amel.abdululazim.se@gmail.com>
2023-09-25 17:30:11 -0700

    task

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _strstr - prints the consecutive
+ * @haystack:string
+ * @needle:string
+ *
+ * Return:string.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		if ((*haystack == *needle && coincidence(haystack, needle) == 1) || !*needle)
+		{
+			return (haystack);
+		}
+		else
+		{
+			haystack++;
+		}
+	}
+	return (0);
+}
+
+/**
+ * coincidence - defintion
+ * @a:string
+ * @b: string
+ *
+ * Return: 1 if there is coincidence, otherwise 0.
+ */
+
+int coincidence(char *a, char *b)
+{
+	while (*b && *b == *a)
+	{
+		b++;
+		a++;
+	}
+
+	if (*b == '\0')
+		return (1);
+	else
+		return (0);
+}
