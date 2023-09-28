b8dd3e18f7b99b2133c3c9f7d9375f5307503285
EngAmel <amel.abdulazim.se@gmail.com>
2023-09-28 19:34:33 +0000

    task

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - calcolate sqr
+ *  @n: number 
+ * 
+ * Return:square
+ * */
+
+int _sqrt_recursion(int n)
+{
+	_calcolate_sqr(0 , n);
+}
+
+/**
+ * _calcolate_sqr- returns square root of a number
+ * @n: number to calculate the square root of
+ * 
+ * Return: the resulting square root
+ **/
+
+int _calcolate_sqr(int sqr  , int n)
+{
+	if (n < 0)
+		return (-1);
+	if (sqr * sqr > n)
+		return (-1);
+	if (sqr * sqr == n)
+		return (sqr);
+	_calcolate_sqr(sqr + 1 , n);
+}
