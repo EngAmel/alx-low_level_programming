3bdb74776f5a341b45d8cb3ed7da1896e41f7b33
EngAmel <amel.abdululazim.se@gmail.com>
2023-09-29 18:29:04 -0700

    task

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -20,5 +20,6 @@ int is_palindrome(char *s);
 int is_palindrome(char *s);
 int check_pal(char *s, int i, int len);
 int _strlen_recursion(char *s);
+int wildcmp(char *s1, char *s2);
 
 #endif /* MAIN_H */
