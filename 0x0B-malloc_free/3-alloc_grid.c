2c43f649f9adf7ca8f83e8df57ff0428de355f15
EngAmel <amel.abdululazim.se@gmail.com>
2023-10-05 07:06:43 -0700

    task

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * **alloc_grid - retun pointer
+ * @width:input width
+ * @height:input height
+ *
+ * Return: pointer
+ **/
+
+int **alloc_grid(int width, int height)
+{
+	int **t, h, w;
+
+	t = malloc(sizeof(*t) * hieght);
+	if (width <= 0 || hieght <= 0 || t == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (h = 0; h < height; h++)
+		{
+			t[h] = malloc(sizeof(**t) * width);
+			if (t[h] == 0)
+			{
+				for (; h--; )
+					free(t[h]);
+				free(t);
+				return (NULL);
+			}
+			for (w = 0; w < width; w++)
+			{
+				t[h][w] = 0;
+			}
+		}
+	}
+	return (t);
+}
+
