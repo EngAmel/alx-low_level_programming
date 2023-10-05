8be31c6b59fd934588fb32f84f76d66214776ae2
EngAmel <amel.abdululazim.se@gmail.com>
2023-10-05 02:17:18 -0700

    task

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *create_array - creating array
+ * @size:size of array
+ * @c:element of array
+ *
+ * Return: the length
+ * */
+
+char *create_array(unsigned int size, char c)
+{
+	char *x = malloc(size);
+
+	if (size == 0 || x == 0)
+		return (0);
+	for (;size--;)
+		x[size] = c;
+	return (x);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+nclude "main.h"
+
+/**
+ * *_strdup - retun pointer
+ * @str:input string
+ *
+ * Return: pointer
+ **/
+char *_strdup(char *str)
+{
+	int i = 0, size = 0;
+	char *mal;
+
+	if (str == NULL)
+		return (NULL);
+	for (; str[size != '\0']; size++)
+		;
+	mal = malloc(size * sizeof(*str) + 1);
+	if (mal == 0)
+		return (NULL);
+	else
+	{
+		for (; i < size; i++)
+			mal[i] = str[i];
+	}
+	return (mal);
+}
