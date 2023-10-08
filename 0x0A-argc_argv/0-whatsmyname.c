af200e426e31d45e01e8710f217e90405d5fb54e
EngAmel <amel.abdululazim.se@gmail.com>
2023-10-07 17:41:31 -0700

    task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -11,6 +12,6 @@
 
 int main(int argc __atribute__((unused)), char const *argv[])
 {
-	printf("%s \n", argv[argc - 1]);
+	printf("%s \n", argv[0]);
 	return (0);
 }
