7cfd2e9d7d68c69f8fe137510d89cd60f8914df7
EngAmel <amel.abdululazim.se@gmail.com>
2023-10-07 19:40:42 -0700

    task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description:C programm use argc and argv
+ * @argv:valu
+ * @argc:counter
+ * Return:Always 0 (Success)
+ **/
+
+int main(int argc, char const *argv[])
+{
+	int res;
+
+	if (argc == 3)
+	{
+		res = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", res);
+	}
+	else
+	{
+		printf("missing args\n");
+		return (1);
+	}
+	return (0);
+}
+
