8c6eda548a3b0aff992f9dc17e1e808a4c6b6a60
EngAmel <amel.abdululazim.se@gmail.com>
2023-10-07 19:05:04 -0700

    task

---
+++ 0x0A-argc_argv/2-str_concat.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:C programm use argc and argv
+ * @argv:valu
+ * @argc:counter
+ * Return:Always 0 (Success)
+ **/
+
+int main(int argc, char const *argv[])
+{
+	int i = 0;
+
+	for (; argc--; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
