0e354d0c01a4371a955560c6932636636d566db5
EngAmel <amel.abdululazim.se@gmail.com>
2023-10-07 18:24:13 -0700

    task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:C programm use argc and argv
+ * @argv:valu
+ * @argc:counter
+ * Return:Always 0 (Success)
+**/
+
+int main(int argc, char const *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
