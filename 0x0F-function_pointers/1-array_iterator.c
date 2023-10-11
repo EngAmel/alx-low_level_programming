9a37fe35e4be065acf2521eb3aa8b504f36fd30f
EngAmel <amel.abdululazim.se@gmail.com>
2023-10-11 15:38:09 -0700

    task

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,7 +10,7 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array && action)
+	if (array && size && action)
 	for (int i = 0; i <= size; i++)
 		action(array[i]);
 }
