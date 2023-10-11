a7fd1d9537d45ef434314104e0e41f3a5a1325c8
EngAmel <amel.abdululazim.se@gmail.com>
2023-10-10 19:59:58 -0700

    task

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - initialing struct dog
+ * @d: dog
+ * @name:name
+ * @age: age
+ * @owner: owner
+ **/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
