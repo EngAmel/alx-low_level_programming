b20410b5d355e23d22b7ef024a990a4fb0b158e8
EngAmel <amel.abdululazim.se@gmail.com>
2023-10-11 19:53:41 -0700

    task

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * main - entry Point
+ *
+ * @argc:input1
+ * @argv:input2
+ *
+ * Return: 0 for succ
+ **/
+
+int main(int argc, char **argv)
+{
+	char *p = (char *)main;
+	int b;
+	
+	if (argc != 2)
+		printf("Error\n"), exit(1);
+	b = atoi(argv[1]);
+	if (b < 0)
+		printf("Error\n"), exit(2);
+	while (b--)
+		printf("%02hhx%s", *p++, b ? " " : "\n");
+	return (0);
+}
