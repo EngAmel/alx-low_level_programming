e8f87b0d207d746f336437f20821bbcc54c713a8
EngAmel <amel.abdululazim.se@gmail.com>
2023-10-11 19:09:04 -0700

    task

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,8 +2,8 @@
 
 /**
  * *get_op_func - function give operation
- * @a:input1
- * @b:input2
+ * @s:input1
+ *
  * Return: int
  **/
 
@@ -18,6 +18,7 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}
 	};
 	int i = 0;
+
 	while (i < 5)
 	{
 		if (s && s[0] == ops[i].op[0] && !s[1])
