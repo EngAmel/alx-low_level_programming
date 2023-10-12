b3a4803a0a2b95b4b4666a4d3fee8747c1ae21af
EngAmel <amel.abdululazim.se@gmail.com>
2023-10-11 19:04:30 -0700

    task

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - function give add
+ * @a:input1
+ * @b:input2
+ * Return: int
+ **/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - give sub
+ * @a:input1
+ * @b:input2
+ * Return: int
+ **/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function give mul..
+ * @a:input1
+ * @b:input2
+ * Return: int
+ **/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function give div
+ * @a:input1
+ * @b:input2
+ * Return: int
+ **/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - function give mod.
+ * @a:input1
+ * @b:input2
+ * Return: int
+ **/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
