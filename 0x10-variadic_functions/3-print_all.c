6ed9f96cb7b44716334b298e06bc2ee6dd5efa53
EngAmel <amel.abdululazim.se@gmail.com>
2023-10-12 19:54:20 -0700

    str

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,38 @@
+#include <string.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - print_numbers
+ * @format:type of inputs
+ *
+ **/
+void print_all(const char *format, ...)
+{
+	va_list args;
+	va_start(args, format);
+	int i = 0;
+
+	while (format[i] != '\0')
+	{
+		if (i > 0)
+			printf(", ");
+		switch (format[i])
+		case 'c':
+			printf("%c", va_arg(args, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(args, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(args, double));
+			break;
+		case 's':
+			printf("%s", va_arg(args, char *));
+			break;
+		default:
+			break;
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
