667d566d08dd608d9793c0220e81c968b163f5ee
EngAmel <amel.abdululazim.se@gmail.com>
2023-10-19 18:03:50 -0700

    print_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "list.h"
+/**
+ * print_list - function that prints all the elements of a list_t list
+ * @h:header
+ *
+ * Return: elm.
+ **/
+
+size_t print_list(const list_t *h)
+{
+	size_t elm = 0;
+	while (h)
+	{
+		if (h-> str == NULL)
+		{
+			h->len = 0;
+			printf("[%d] (nill)\n", h->len);
+		}
+		else 
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			elm = elm + 1;
+			h = h->next;
+		}
+		return (elm);
+	}
+}
