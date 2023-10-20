ad87a12f37b746c5e129041724e04fb52f2c9e33
EngAmel <amel.abdululazim.se@gmail.com>
2023-10-19 18:10:11 -0700

    print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,15 @@
 size_t print_list(const list_t *h)
 {
 	size_t elm = 0;
+
 	while (h)
 	{
-		if (h-> str == NULL)
+		if (h->str == NULL)
 		{
 			h->len = 0;
 			printf("[%d] (nill)\n", h->len);
 		}
-		else 
+		else
 		{
 			printf("[%d] %s\n", h->len, h->str);
 			elm = elm + 1;
