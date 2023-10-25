ff764d23e53e53032f04e0175fe7489fb28985b4
EngAmel <amel.abdululazim.se@gmail.com>
2023-10-24 18:22:56 -0700

    listint

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,19 +1,18 @@
 #include "lists.h"
 
 /**
- * print_listint - function that prints all the elements of a list_t listint
+ * listint_len - function that prints all the elements of a list_t listint
  * @h:header
  *
  * Return: i.
  **/
 
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t i = 0;
 
 	for (; h != NULL ;)
 	{
-		printf("%d\n", h->n);
 		i++;
 		h = h->next;
 	}
