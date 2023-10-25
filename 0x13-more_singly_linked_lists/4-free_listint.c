3a744224107844b4af6433ee531642e1b51d711a
EngAmel <amel.abdululazim.se@gmail.com>
2023-10-25 16:30:56 -0700

    listint

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint - function that frees list_t listint
+ *   * @head:header
+ *
+ **/
+void free_listint(listint_t *head)
+{
+	listint_t *t;
+
+	while (head)
+	{
+		t = head->next;
+		free(head);
+		head  = t;
+	}
+}
