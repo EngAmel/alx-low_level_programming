a25ba1a591d23965601f22e08b2fdf1b19af2ff3
EngAmel <amel.abdululazim.se@gmail.com>
2023-10-26 16:17:24 -0700

    bits

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - convertting
+ * @b: pointer
+ *
+ * Return: unsigned int
+ *       */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int n = 0, i = 1;
+	int ln;
+
+	if (b == '\0')
+		return (0);
+
+	for (ln = 0; b[ln];)
+		ln++;
+
+	for (ln -= 1; ln >= 0; ln--)
+	{
+		if (b[ln] != '0' && b[ln] != '1')
+			return (0);
+
+		n += (b[ln] - '0') * i;
+		mult *= 2;
+	}
+
+	return (n);
+}
