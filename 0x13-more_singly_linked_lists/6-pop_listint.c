ffb3d90304e4d26e6a802cfefb9504db9a44b447
EngAmel <amel.abdululazim.se@gmail.com>
2023-10-25 18:14:03 -0700

    listint

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ *  * free_listint2 - function that frees list_t listint
+ *   * @head:header
+ *    *
+ *     **/
+int pop_listint(listint_t **head)
+{
+		listint_t *t;
+
+			while (*head)
+					{
+								t = (*head)->next;
+										free(*head);
+												*head  = t;
+													}
+				*head = NULL;
+
+}
