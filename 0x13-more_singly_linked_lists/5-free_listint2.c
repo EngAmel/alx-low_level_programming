80ac47b93f58a42c0422e29c76610b4aa4688861
EngAmel <amel.abdululazim.se@gmail.com>
2023-10-25 17:05:05 -0700

    listint

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,15 +9,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *t;
 
-	if (*head == NULL)
-		printf("(nil)");
 
-	else
-		while (*head)
-		{
-			t = (*head)->next;
-			free(*head);
-			*head  = t;
-		}
+	while (*head)
+	{
+		t = (*head)->next;
+		free(*head);
+		*head  = t;
+	}
 	*head = NULL;
 }
