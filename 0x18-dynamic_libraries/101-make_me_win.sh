eceeb54eac81c96562e76776aaf448212c4974e1
EngAmel <amel.abdululazim.se@gmail.com>
2023-12-12 03:14:02 +0300

    task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/EngAmel/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD= libgiga.so
+wget -P /tmp https://github.com/EngAmel/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD=/tmp/libgiga.so
